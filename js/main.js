21599712179dee8555a1d12d2ddfeb37ccbc7443
omar kazoum <omarKazoum96@gmail.com>
2022-01-17 22:09:21 +0100

    some improvement in the sidebar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 var hidden=false;
+$('.btn-toggle-sidebar').css("transform", "rotate(90deg)");
 $(function(){
     $('.btn-toggle-sidebar').on("click",function(){
-        $('.sidebar').toggle();
 
+        $('.sidebar').slideToggle(1000);
         if(hidden) {
             $(".content").removeClass('col-12');
             $(".content").addClass('col-9');
-            $('.btn-toggle-sidebar').css("transform", "rotate(0deg)");
+            $('.btn-toggle-sidebar').css("transform", "rotate(90deg)");
         }else{
-            $('.btn-toggle-sidebar').css("transform", "rotate(180deg)");
+            $('.btn-toggle-sidebar').css("transform", "rotate(-90deg)");
             $(".content").removeClass('col-9');
             $(".content").addClass('col-12');
         }
