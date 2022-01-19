13be6bf7f4f82bb475fd4705f7c689e6182714ab
omar kazoum <omarKazoum96@gmail.com>
2022-01-19 17:45:03 +0100

    fixed the problem in xs devices

--- students.php
+++ students.php
@@ -11,7 +11,7 @@
 <main class="container-fluid bg-gray">
     <div class="row">
         <?php include 'sidebar.php';?>
-        <div class="col content">
+        <div class="col-9 content">
             <?php include 'header.php';?>
             <div class="main-content row p-2 d-flex align-items-center">
                 <div class="col-12 main-content-toolbar d-flex pb-2 justify-content-between align-items-center border-bottom-light">
@@ -42,20 +42,21 @@
                     <?php
                     // let's fill the array with the students data
                     for($i=1;$i<=10;$i++)
-                    $students[]=array(
-                        'name'=>'name '.$i,
-                        'email'=>'name'.$i.'@domain.ext',
-                        'phone'=>'212677886655',
-                        'enrolNbr'=>1000+$i,
-                        'dateAdmission'=>$i.'-Dec, 2021s'
-                    );
+                        $students[]=array(
+                            'name'=>'name '.$i,
+                            'email'=>'name'.$i.'@domain.ext',
+                            'phone'=>'212677886655',
+                            'enrolNbr'=>1000+$i,
+                            'dateAdmission'=>$i.'-Dec, 2021s');
                     // now let's print the data
                     foreach($students as $student){
                     ?>
                     <div class="col-12">
                     <div class="card shadow">
-                        <div class="card-body row d-flex flex-column flex-md-row">
-                            <img src="images/student-img.jfif" alt="" class="card-image col-lg-1">
+                        <div class="card-body d-flex flex-column flex-md-row">
+                            <span class="col-lg-1 col-auto">
+                                <img src="images/student-img.jfif" alt="" class="w-100">
+                            </span>
                             <span class="col-lg-2 text-start">
                                 <?php echo $student['name'];?>
                             </span>
