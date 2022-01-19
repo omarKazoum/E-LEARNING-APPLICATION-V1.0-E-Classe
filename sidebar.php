8bcc826095aa2ea374137fb60d47a289646e1156
omar kazoum <omarKazoum96@gmail.com>
2022-01-19 01:59:16 +0100

    now navbar active item color changes.

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,4 @@
+<?php $active_class='bg-primary'; ?>
 <div class="toggle col-3 sidebar p-1 p-md-3">
     <h1 class="text-left border-start border-3 border-md-4 border-primary ps-md-2 fw-bold fs-4 mb-3 mb-md-0">E-classe</h1>
     <div class="w-100 px-md-5 mx-md-auto d-flex flex-column align-items-center mt-md-5 mb-md-2">
@@ -6,7 +7,7 @@
         <h6 class="text-primary text-bold mt-3 d-none d-md-block">Admin</h6>
     </div>
     <div class="menu-options-con row w-100 px-lg-5 mx-auto py-md-5 py-2 mt-3 mt-md-0 ">
-        <a href="Dashboard.php" class="btn gap-2 bg-primary mb-4 d-flex justify-content-md-between justify-content-center w-100 align-items-center">
+        <a href="Dashboard.php" class="btn gap-2 <?php if(basename($_SERVER['SCRIPT_NAME'])=='Dashboard.php')echo $active_class ?> mb-4 d-flex justify-content-md-between justify-content-center w-100 align-items-center">
             <span class="ic ic-home"></span>
             <p class="w-75 text-start m-0 d-none d-md-block">
                 Home
@@ -18,13 +19,13 @@
                 Course
             </p>
         </a>
-        <a href="students.php" class="btn gap-2  mb-4 d-flex justify-content-md-between justify-content-center w-100 align-items-center">
+        <a href="students.php" class="btn gap-2 <?php if(basename($_SERVER['SCRIPT_NAME'])=='students.php')echo $active_class ?> mb-4 d-flex justify-content-md-between justify-content-center w-100 align-items-center">
             <span class="ic ic-students"></span>
             <p class="w-75 text-start m-0 d-none d-md-block">
                 Students
             </p>
         </a>
-        <a href="payments.php" class="btn gap-2  mb-4 d-flex justify-content-md-between justify-content-center w-100 align-items-center">
+        <a href="payments.php" class="btn gap-2 <?php if(basename($_SERVER['SCRIPT_NAME'])=='payments.php')echo $active_class ?> mb-4 d-flex justify-content-md-between justify-content-center w-100 align-items-center">
             <span class="ic ic-payment my-md-0 my-1"></span>
             <p class="w-75 text-start m-0 d-none d-md-block">
                 Payment
